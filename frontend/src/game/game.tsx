7a86be64ce9b274b641096302c862da3dc26aaf4
Cadecraft <cadendotlee@gmail.com>
2025-01-25 15:05:52 -0800

    watercolor map

--- frontend/src/game/game.tsx
+++ frontend/src/game/game.tsx
@@ -35,12 +35,14 @@ function MyMap(props: {
             >
                 <ChangeView center_first={props.center_first} center_second={props.center_second} zoom={props.zoom} />
                 <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution = '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url = 'https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg'
                 />
             </MapContainer>
         </div>
     )
+    // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 }
 
 export default function GamePage(){
