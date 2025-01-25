c0f8ec3fd1ed4826f23b907f2439b5f75f476bd1
Dylan Liu <liudj5@uci.edu>
2025-01-24 22:10:28 -0800

    Update index.js
    continued backend set up

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,19 @@ const http = require('http');
 const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
-
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST"],
+    },
+  });
 io.on('connection', (socket) => {
-  console.log('a user connected');
-});
+    console.log('a user connected');
+    socket.on('hello',(count)=>{
+        console.log(count + 1);
+    })
+  });
   
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
