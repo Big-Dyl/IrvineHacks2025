0612e282366f26ec22c39a89cef40c188f7f6489
edokydocy <edokydocy@gmail.com>
2025-01-25 10:55:59 -0800

    Added home page skeleton
    No connection to backend code yet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+import { ComboboxDemo } from './game/citybox'
+import { Button } from "@/components/ui/button"
+
+import Pioneer from "./assets/PeterAnteater-new_canvas.png"
+import Rouge from "./assets/Rouge.png"
+import Peter from "./assets/Peter_AntEater.png"
+
 
 function App() {
   const [selectedChar, setSelectedChar] = useState(0);
@@ -23,40 +33,46 @@ function App() {
     window.location.href = newUrl.toString();
   };
 
+  // select character
+  const [choice, setChoice] = useState(0);
+
   return (
     <>
-      <h1>SpeedStreets</h1>
-      <div className="card">
-        <div>
+      <div className='mx-auto w-4/5 h-screen border-4 border-blue border-dashed'>
+        <h1 className='font-bold text-white text-6xl flex justify-center mt-24 border-t-20 py-4 border-b-20'>- - - - - - - - -  🧐 SpeedStreets 🏙  - - - - - - - - -</h1>
+        <div id='inputs' className='flex items-end justify-center mx-auto my-18 w-1/2 h-200px'>
+          
+          <Input
+            className='border-4 h-12 bg-white'
+            type='text'
+            placeholder='Enter your name'
+            style={{width: 180}}
+          />
+          <div className='ml-20'>
+            <Label htmlFor='roomcode' className='text-white'>Enter a room code to join:</Label>
+            <Input
+              id='roomcode'
+              placeholder='Roomcode'
+              className='border-4 h-12 bg-white'
+              style={{width: 180}}
+            />
+          </div>
+          <Button className='bg-black text-white h-12 ml-2'>Hop on!</Button>
+
+          <div className='ml-20'>
+            <Label htmlFor='' className='text-white shadow-lg'>Or select a city to host:</Label>
+            <ComboboxDemo/>
+          </div>
+          <Button className='bg-black text-white h-12 ml-2'>Host it!</Button>
+
         </div>
-        <br />
-        <div>
-          Select your character:
-          <div>
-            <button style={selectedChar == 0 ? {} : {"backgroundColor": "black"}} onClick={() => setSelectedChar(0)}>Peter</button>
-            <button style={selectedChar == 1 ? {} : {"backgroundColor": "black"}}  onClick={() => setSelectedChar(1)}>Pioneer</button>
-            <button style={selectedChar == 2 ? {} : {"backgroundColor": "black"}}  onClick={() => setSelectedChar(2)}>Rogue</button>
-          </div>
-        </div>
-        <br />
-        <br />
-        <div style={{"display": "flex", "alignItems": "center"}}>
-          <div>
-            <input placeholder="Room Code" value={roomCode} onChange={e => setRoomCode(e.target.value)} />
-            <br />
-            <button onClick={joinGame}>Join Game!</button>
-          </div>
-          <div style={{"margin": "20px"}}>
-            OR
-          </div>
-          <div>
-            <button onClick={hostGame}>Host Game!</button>
-          </div>
+        <div className='font-sans text-white text-2xl flex justify-center mt-18'>Select your characters:</div>
+        <div className='flex mx-auto w-4/5 mt-10 justify-center'>
+          <img src={Peter} className={`w-80 h-80 object-contain p-4 ${choice==0? 'rounded-full outline-1' : ''}`} onClick={()=>{setChoice(0)}}/>
+          <img src={Pioneer} className={`w-80 h-80 object-contain p-4 ${choice==1? 'rounded-full outline-1' : ''}` } onClick={()=>{setChoice(1)}}/>
+          <img src={Rouge} className={`w-80 h-80 object-contain p-4 ${choice==2? 'rounded-full outline-1' : ''}` } onClick={()=>{setChoice(2)}}/>
         </div>
       </div>
-      <p className="read-the-docs">
-        Created for IrvineHacks 2025
-      </p>
     </>
   );
 }
---
+++ frontend/src/game/citybox.tsx
@@ -0,0 +1,94 @@
+"use client"
+
+import * as React from "react"
+import { Check, ChevronsUpDown } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
+const cities = [
+  {
+    value: "irvine",
+    label: "Irvine",
+  },
+  {
+    value: "Los Angeles",
+    label: "Los Angeles",
+  },
+  {
+    value: "nuxt.js",
+    label: "Nuxt.js",
+  },
+  {
+    value: "remix",
+    label: "Remix",
+  },
+  {
+    value: "astro",
+    label: "Astro",
+  },
+]
+
+export function ComboboxDemo() {
+  const [open, setOpen] = React.useState(false)
+  const [value, setValue] = React.useState("")
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] h-12 border-4 justify-between bg-white"
+        >
+          {value
+            ? cities.find((city) => city.value === value)?.label
+            : "Select city..."}
+          <ChevronsUpDown className="opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0 bg-white">
+        <Command>
+          <CommandInput placeholder="Search city..." className="h-12" />
+          <CommandList>
+            <CommandEmpty>No city found.</CommandEmpty>
+            <CommandGroup>
+              {cities.map((city) => (
+                <CommandItem
+                  key={city.value}
+                  value={city.value}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue === value ? "" : currentValue)
+                    setOpen(false)
+                  }}
+                >
+                  {city.label}
+                  <Check
+                    className={cn(
+                      "ml-auto",
+                      value === city.value ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  )
+}
