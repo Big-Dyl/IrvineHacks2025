75600da019b9c374c5b2cb6d9d92a24ed185822c
Dylan Liu <liudj5@uci.edu>
2025-01-25 23:28:39 -0800

    game pauses when it is over

---
+++ frontend/src/game/endscreen.tsx
@@ -0,0 +1,8 @@
+import theimage from "../assets/Podeum.png";
+export function EndScreen(){
+    return(
+        <>
+            <img src={theimage} style={{width:"80vh" ,"height": "60vh"}}/>
+        </>
+    )
+}
