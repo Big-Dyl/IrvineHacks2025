22f598e9d04a072929771ec668240c13eca35c9c
Jianan Zhu <jianaz11@uci.edu>
2025-01-25 17:02:01 -0800

    Stuff

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App";
 import GamePage from "./game/game.tsx";
+import {TextArea} from "./game/textchannel.tsx";
 
 const root = document.getElementById("root");
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(root).render(
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="game" element={<GamePage />} />
+      <Route path="texta" element={<TextArea/>}/>
     </Routes>
   </BrowserRouter>
 );
